00bc76cbd1c9eaab4bf711bde12a8b2f4ee912b6
root <you@example.com>
2021-06-19 15:52:42 -0700

    Variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,21 +10,21 @@ int main(void)
 
 int j, i;
 
-for (j = 0; j <= 99; j++)
+for (i = 0; i <= 98; i++)
 {
-for (i = j; i <= 99; i++)
+for (j = i + 1; j <= 99; j++)
 {
 if (i != j)
 
 {
-putchar(j / 10 + 48);
-putchar(j % 10 + 48);
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar(i / 10 + 48);
-putchar(i % 10 + 48);
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 }
 
-if (j * 100 + i != 9899)
+if ((i == 98) && (j == 99))
 
 {
 putchar(',');
